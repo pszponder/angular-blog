1ff698e83185175b66d9b755ccb36bdeb5c74f59
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-27 10:17:17 -0700

    added components and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TodoAddComponent } from './components/todo-add/todo-add.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-list/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    TodoAddComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
