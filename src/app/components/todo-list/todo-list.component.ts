dca5d127168ecafc8b59e252fc3cb7ff3c328bd9
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-27 19:29:17 -0700

    imported initial dummy data from todos service to todo-list component and passed into each todo-item

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
+import { Todo } from 'src/app/shared/todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
+    this.todos = this.todosService.todoList;
   }
-
 }
