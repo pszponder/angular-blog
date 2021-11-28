9d8b14f293b906f201a951f953911a9d757c4b89
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-27 19:16:52 -0700

    complete initial todos service with data and functionality, implemented todo interface

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,85 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../shared/todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
+  constructor() {}
 
-  constructor() { }
+  _todoList: Todo[] = [
+    {
+      id: 0,
+      description: '1st Todo: COMPLETE',
+      important: false,
+      complete: true,
+    },
+    {
+      id: 1,
+      description: '2nd Todo',
+      important: false,
+      complete: false,
+    },
+    {
+      id: 2,
+      description: '3rd Todo: IMPORTANT',
+      important: true,
+      complete: false,
+    },
+    {
+      id: 3,
+      description: '4th Todo: IMPORTANT & COMPLETE',
+      important: true,
+      complete: true,
+    },
+    {
+      id: 4,
+      description: '5th Todo: IMPORTANT',
+      important: true,
+      complete: false,
+    },
+  ];
+
+  // Getter for _todoList
+  get todoList() {
+    return this._todoList;
+  }
+
+  // TODO: Determine if we need this
+  // Setter for _todoList
+
+  // Add a TODO item to the list
+  addTodo(description: string): void {
+    this._todoList.push({
+      id: this._todoList.length,
+      description: description,
+      important: false,
+      complete: false,
+    });
+  }
+
+  // Edit a TODO item description at a specific index
+  editTodo(newDescription: string, todoId: number): void {
+    this._todoList[todoId].description = newDescription;
+  }
+
+  // Toggle the important property of a Todo of specified index
+  toggleTodoImportant(todoId: number): void {
+    this._todoList[todoId].important = !this._todoList[todoId].important;
+  }
+
+  // Toggle the complete property of a Todo of specified index
+  toggleTodoComplete(todoId: number): void {
+    this._todoList[todoId].complete = !this._todoList[todoId].complete;
+  }
+
+  // Remove a Todo at a specified index
+  RemoveTodo(todoId: number): void {
+    this._todoList.splice(todoId, 1);
+  }
+
+  // Clear the TODO data set
+  clearTodos(): void {
+    this._todoList = [];
+  }
 }
---
+++ src/app/shared/todo.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+  id: number;
+  description: string;
+  important: boolean;
+  complete: boolean;
+}
