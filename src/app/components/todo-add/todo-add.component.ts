ccf2346701f6cdf2dd1dce1141c7924288c7d007
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-11-27 22:22:57 -0700

    complete add functionality in the todo-add component

--- src/app/components/todo-add/todo-add.component.ts
+++ src/app/components/todo-add/todo-add.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TodosService } from 'src/app/services/todos.service';
+import { Todo } from 'src/app/shared/todo';
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
-  styleUrls: ['./todo-add.component.css']
+  styleUrls: ['./todo-add.component.css'],
 })
 export class TodoAddComponent implements OnInit {
+  // Declare todoList method which will be a reference to the data from the todos service
+  _todoList: Todo[] = [];
 
-  constructor() { }
+  // Declare addTodo method
+  addTodo(description: string): void {}
+
+  constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
-  }
+    this._todoList = this.todosService.todoList;
 
+    // Set the component's addTodo method to be a copy of todosService method
+    this.addTodo = this.todosService.addTodo;
+  }
 }
